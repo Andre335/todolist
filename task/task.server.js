0a9f7e8c08b399a8fc2e6c4a5823ceaab50c4a9c
Andre Goncalves <andre335@gmail.com>
2019-03-30 18:01:15 -0300

    Task Server

--- task/task.server.js
+++ task/task.server.js
@@ -1,1 +1,24 @@
-// Task Server
+"use strict"
+
+const Task = require("./task.model");
+
+exports.findAll = async (data) => {
+    return await Task.find({});
+};
+
+exports.findOne = async (id) => {
+    return await Task.findById(id);
+};
+
+exports.create = async (data) => {
+    const task = new Task(data);
+    await task.save();
+};
+
+exports.deleteById = async (id) => {
+    await task.findByIdAndDelete(id);
+};
+
+exports.drop = async () => {
+    await Task.collection.drop();
+};
