9dc0e40b9e3bcd564ebb3fa847c308d3edb543b8
Andre Goncalves <andre335@gmail.com>
2019-03-30 17:54:01 -0300

    Task Router + App.js config

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-// App.js
+const express = require('express');
+var task = require('./task/task.routes');
+var configs = require('./credentials.json');
+var mongoose = require('mongoose');
+
+const bodyParser = require('body-parser');
+const app = express();
+const PORT = configs.PORT || 3001;
+
+mongoose.connect('mongodb://' + configs.DBUSER + ':' + configs.DBPASS + '@ds229373.mlab.com:29373/gbet', { useNewUrlParser: true });
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use('/task', task);
+
+app.listen(PORT, () => console.log('Server started on port ' + PORT))
+module.exports = app;
--- task/task.routes.js
+++ task/task.routes.js
@@ -1,1 +1,19 @@
-// Task Routes
+const express = require('express')
+var Task = require('./task.controller')
+var router = express.Router()
+
+// Return All Tasks
+router.get('/', Task.findAll)
+
+// Return One Task by ID
+router.get('/:id', Task.findOne)
+
+// Create One Task
+router.post('/', Task.create)
+
+// Delete Task by ID
+router.delete('/:id', Task.deleteById)
+
+module.exports = router
+
+
