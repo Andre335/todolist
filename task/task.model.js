2c7e95c5ad6d3af028423a210c13de201d51d7c4
Andre Goncalves <andre335@gmail.com>
2019-03-30 18:06:14 -0300

    Task Model

--- task/task.model.js
+++ task/task.model.js
@@ -1,1 +1,27 @@
-// Task Model
+'use strict'
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var taskSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    type: {
+        type: String,
+        required: true
+    },
+    period: {
+        type: Date,
+        required: true
+    },
+    status: {
+        type: Boolean,
+        required: true,
+        default: false
+    }
+});
+
+var Task = mongoose.model('Task', taskSchema);
+module.exports = Task;
